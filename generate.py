cea3cbd58b8c073251d1f50bc189f42403da7a15
Meleshchenia Ksenia <ksenialex03@gmail.com>
2021-03-29 16:06:35 +0300

    PEP8

--- generate.py
+++ generate.py
@@ -4,18 +4,28 @@ import pickle
 import re
 from textblob import TextBlob
 
-#Обработка флагов
+
 @click.command()
-@click.option('--model', required=True)#Путь к файлу, из которого загружается модель
-@click.option('--seed', default='.', required=False)#необязательный аргумент. Начальное слово. Если не указано, слово выбирается случайно из всех слов (не учитывая частоты).
-@click.option('--length', required=True, type=int)#Длина генерируемой последовательности
-@click.option('--output', default='stdout', required=False)#Необязательный аргумент. Файл, в который будет записан результат. Если аргумент отсутствует, вывод производиться в stdout
+@click.option('--model', required=True)
+@click.option('--seed', default='.', required=False)
+@click.option('--length', required=True, type=int)
+@click.option('--output', default='stdout', required=False)
 def generate(model, seed, length, output):
-    #Загрузка модели из файла 	
-    with open(model, 'rb') as f:
-        mod = pickle.load(f)
-    
-    #Выбор первого слова	
+    """
+    Функция генерирует последовательность слов длины length.
+    Словарь находится в model.
+    Начальное слово -- seed.
+    Результат выводится в output(по умолчанию stdout).
+    :param model: Файл со словарём
+    :param seed: Начальное слово
+    :param length: Длина генерируемой последовательности
+    :param output: Файл для вывода
+    """
+    # Загрузка модели из файла
+    with open(model, 'rb') as file:
+        mod = pickle.load(file)
+
+    # Выбор первого слова
     if seed in mod.keys():
         first_word = np.random.choice(list(mod[seed].keys()))
     else:
@@ -23,25 +33,25 @@ def generate(model, seed, length, output):
 
     res = []
     test_chain = [first_word]
-    
-    #Создание цепи нужной длины(по одному слову)    
+
+    # Создание цепи нужной длины(по одному слову)
     while len(test_chain) < length:
-    	words = list(mod[test_chain[-1]].values())
-    	probability = [float(el) / sum(words) for el in words]
+        words = list(mod[test_chain[-1]].values())
+        probability = [float(el) / sum(words) for el in words]
         res = np.random.choice(words, 1, True, probability)
         test_chain.append(res[0])
 
-    #Объединение слов в текст
+    # Объединение слов в текст
     sentence = ' '.join(test_chain)
     sentence = re.sub(' \.', '.', sentence)
 
-    #Вывод в файл из stdout
+    # Вывод в файл из stdout
     if output == 'stdout':
         print(TextBlob(sentence.capitalize()))
     else:
-    	with open(output, 'w') as f 
-            f.write(sentence)
-            f.close()
+        with open(output, 'w') as file:
+            file.write(sentence)
 
-#Вызов функции
+# Вызов функции
 generate()
+
--- train.py
+++ train.py
@@ -4,59 +4,58 @@ import pickle
 import re
 
 
-#Генератор линий из текста
+# Генератор линий из текста
 def lines(name):
     if name == "stdin":
         yield input()
-    for f in open(name, 'r', encoding='utf-8'):
-        yield f
-
-#Обновление словаря
-def update(dictionary, item):
-    if item in dictionary:
-        dictionary[item] += 1
-    else:
-        dictionary[item] = 1
+    for line in open(name, 'r', encoding='utf-8'):
+        yield line
 
 
 @click.command()
-#Путь к файлу. Если данный аргумент не задан, тексты будут вводятся из stdin.
 @click.option('--input-dir', default='stdin')
-#Путь к файлу, в который сохраняется модель.
 @click.option('--model')
-#При задании приводит тексты к lowercase.
 @click.option('--lc', default=1, required=False)
 def train(input_dir, model, lc):
+    """
+    Обучает модель для генерации текста на основе цепей Маркова.
+    Файл из которого загружается текстовый источник -- input_dir.
+    Файл в который загружается модель -- model.
+    :param input_dir: Исходник для обучения.
+    :param model: Файл для записи модели.
+    :param lc: Приводить ли текст к lowercase.
+    """
     chain = defaultdict(defaultdict(lambda: 0))
     start = ""
-    
-    #Считывание текста из файла 
-    for f in lines(input_dir):
-        #Приведение текста к lowercase
+
+    # Считывание текста из файла
+    for string in lines(input_dir):
+        # Приведение текста к lowercase
         if lc:
-            f = f.lower()
-            
-        #Чистка линии от лишних символов    
-        f = re.sub(' +', ' ', f)
-        f = re.sub('\.+|\?|!', ' .', f)
-        f = re.sub('[,()]', ' ', f)
-        f = f[:-1]
-        line = [start] + re.split('; |, | ', f)
-            
-        #Создание словаря из пар слов    
+            string = string.lower()
+
+        # Чистка линии от лишних символов
+        string = re.sub(' +', ' ', string)
+        string = re.sub('\.+|\?|!', ' .', string)
+        string = re.sub('[,()]', ' ', string)
+        string = string[:-1]
+        line = [start] + re.split('; |, | ', string)
+
+        # Создание словаря из пар слов
         for i in range(len(line) - 1):
             if line[i] in chain:
-                chain[line[i]][line[i + 1]
+                chain[line[i]][line[i + 1]] += 1
             else:
                 chain[line[i]] = {line[i + 1]: 1}
-        
-        #Сохранение последнего слова для новой линии
+
+        # Сохранение последнего слова для новой линии
         if len(line) > 0:
             start = line[-1]
-    
-    #Запись модели в файл
+
+    # Запись модели в файл
     with open(model, 'wb') as output:
         pickle.dump(chain, output)
 
-#Вызов функции
+# Вызов функции
 train()
+
